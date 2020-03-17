6cadbb7721a8bbbd014d53e0b7589452cf4730c7
Gustavo Passos <theguspassos@gmail.com>
2020-03-16 21:27:45 -0300

    add carousel title

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -9,27 +9,31 @@ import "pure-react-carousel/dist/react-carousel.es.css";
 import ProjectSlide from "./ProjectSlide";
 
 import sketchImage from "../../images/projects/sketch/sketch-carousel.png";
+import CarouselTitle from "./CarouselTitle";
 
 const projectsInCarousel = [{ image: sketchImage, name: "sketch" }];
 
 function Carousel() {
   return (
-    <CarouselProvider
-      naturalSlideWidth={10}
-      naturalSlideHeight={10}
-      visibleSlides={1}
-      totalSlides={2}
-    >
-      <Slider>
-        {projectsInCarousel.map((value, i) => {
-          return (
-            <ProjectSlide index={i} img={value.image} key={i}></ProjectSlide>
-          );
-        })}
-      </Slider>
-      <ButtonBack>back</ButtonBack>
-      <ButtonNext>next</ButtonNext>
-    </CarouselProvider>
+    <div>
+      <CarouselTitle></CarouselTitle>
+      <CarouselProvider
+        naturalSlideWidth={10}
+        naturalSlideHeight={10}
+        visibleSlides={1}
+        totalSlides={2}
+      >
+        <Slider>
+          {projectsInCarousel.map((value, i) => {
+            return (
+              <ProjectSlide index={i} img={value.image} key={i}></ProjectSlide>
+            );
+          })}
+        </Slider>
+        <ButtonBack>back</ButtonBack>
+        <ButtonNext>next</ButtonNext>
+      </CarouselProvider>
+    </div>
   );
 }
 
---
+++ src/components/Carousel/CarouselTitle.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import colors from "../../shared/colors";
+
+const style = {
+  background: colors.mainColor,
+  color: colors.mainTextColor,
+  textAlign: "center" as const,
+  padding: "10px"
+};
+
+function CarouselTitle() {
+  return <div style={style}>those are some of the projects I've worked on</div>;
+}
+
+export default CarouselTitle;
