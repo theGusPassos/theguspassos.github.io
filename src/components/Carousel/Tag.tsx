6b68347e92d86c8d2973296876a5c537c5c9179e
Gustavo Passos <theguspassos@gmail.com>
2020-03-17 19:06:09 -0300

    change tag to float right

--- src/components/Carousel/Tag.tsx
+++ src/components/Carousel/Tag.tsx
@@ -2,16 +2,18 @@ import React from "react";
 import colors from "../../shared/colors";
 
 const style = {
+  fontSize: "0.9em",
   background: colors.mainBackgroundColor,
   textAlign: "center" as const,
   display: "inline-block",
   minWidth: "70px",
-  margin: 10,
-  padding: 5,
+  margin: 5,
+  padding: 3,
   border: "3px solid",
   borderColor: colors.mainColor,
   color: colors.mainColor,
-  borderRadius: "50px"
+  borderRadius: "50px",
+  float: "right" as const
 };
 
 interface TagProps {
