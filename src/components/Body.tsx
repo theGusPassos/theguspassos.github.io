6f426a764f64a637a802bac6645482cd6b182586
Gustavo Passos <theguspassos@gmail.com>
2020-03-23 20:17:40 -0300

    add grid structure to page

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -3,9 +3,15 @@ import Header from "./Header";
 import Carousel from "./Carousel/Carousel";
 import Menu from "./Menu";
 
+const bodyStyle = {
+  display: "grid",
+  gridTemplateRows: "15% 45% 40%",
+  height: "100%"
+};
+
 function Body() {
   return (
-    <div>
+    <div style={bodyStyle}>
       <Header></Header>
       <Carousel></Carousel>
       <Menu></Menu>
--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,7 +1,34 @@
 import React from "react";
+import colors from "../shared/colors";
+
+const navContainer = {
+  color: colors.mainColor
+};
+
+const buttonContainer = {
+  padding: "0"
+};
+
+const normalButton = {
+  fontSize: "1.5em",
+  textAlign: "center" as const,
+  listStyle: "none"
+};
+
+const middleButton = {
+  ...normalButton
+};
 
 function Menu() {
-  return <div>teststets</div>;
+  return (
+    <nav style={navContainer}>
+      <ul style={buttonContainer}>
+        <li style={normalButton}>My other projects</li>
+        <li style={middleButton}>About me</li>
+        <li style={normalButton}>Contact</li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Menu;
