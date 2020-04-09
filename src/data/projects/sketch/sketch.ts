4bf620a98fdb806062d7ae4e7bf7854537d7e617
Gustavo Passos <theguspassos@gmail.com>
2020-04-09 17:06:08 -0300

    add sketch project data

---
+++ src/data/projects/sketch/sketch.ts
@@ -0,0 +1,11 @@
+import Project from "../../../models/project";
+import img from "../../../images/projects/sketch/sketch-carousel.png";
+import imgBig from "../../../images/projects/sketch/sketch-carousel-big.png";
+
+export const sketch: Project = {
+  name: "sketch",
+  tags: ["c#", "Unity"],
+  imageAlt: "sketch gameplay screenshot",
+  image: img,
+  imageBig: imgBig,
+};
