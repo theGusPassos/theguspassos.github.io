c973c1fb535f22abb375a78eac9be912a814f4bb
Gustavo Passos <theguspassos@gmail.com>
2020-04-10 15:46:14 -0300

    add menu to home folder

--- src/components/home/Menu.tsx
+++ src/components/home/Menu.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import { device } from "../shared/device";
+import { device } from "../../shared/device";
 import { Link } from "react-router-dom";
-import LinkButton from "./common/LinkButton";
-import ContactsButton from "./contacts/ContactsButton";
+import LinkButton from "../common/LinkButton";
+import ContactsButton from "../contacts/ContactsButton";
 
 const NavMenu = styled.nav`
   flex: 1;
