266e834ce561f5645e13d71020645551b477a1bb
Gustavo Passos <theguspassos@gmail.com>
2020-04-16 02:07:33 -0300

    Updates

---
+++ precache-manifest.75388a7b775427a5644559083b4d3d82.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3e1d867c9b347a199a86753ff1bc07d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f99730b0d22ab1ed6513",
+    "url": "/static/css/2.735a36f9.chunk.css"
+  },
+  {
+    "revision": "6b8b61847ef3df5de86d",
+    "url": "/static/css/main.b3b2aeda.chunk.css"
+  },
+  {
+    "revision": "f99730b0d22ab1ed6513",
+    "url": "/static/js/2.fc982e08.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fc982e08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b8b61847ef3df5de86d",
+    "url": "/static/js/main.8502e6f2.chunk.js"
+  },
+  {
+    "revision": "5da85dcf5f4ca828e9ad",
+    "url": "/static/js/runtime-main.36844e49.js"
+  },
+  {
+    "revision": "de40467c81fa28189c2ae649c9d291dd",
+    "url": "/static/media/EubyKuma.de40467c.png"
+  },
+  {
+    "revision": "9f96cfd196b18ce38995bec84b0bd2c8",
+    "url": "/static/media/github.9f96cfd1.png"
+  },
+  {
+    "revision": "62549d5c53693231006fa6bb8705c65d",
+    "url": "/static/media/gmail.62549d5c.png"
+  },
+  {
+    "revision": "072650574e041bf951d5e4e5ce07d86f",
+    "url": "/static/media/linkedin.07265057.png"
+  },
+  {
+    "revision": "86fcbb81497b13f0c7a0c5025d50c354",
+    "url": "/static/media/sketch-carousel-big.86fcbb81.png"
+  },
+  {
+    "revision": "7b5eb98fdd85f38ca80f8dfb3c2736a7",
+    "url": "/static/media/sketch-carousel.7b5eb98f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8ca8d4af62489a7e0af06976357eb626.js"
+  "/precache-manifest.75388a7b775427a5644559083b4d3d82.js"
 );
 
 self.addEventListener('message', (event) => {
