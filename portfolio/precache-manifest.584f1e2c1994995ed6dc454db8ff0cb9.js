75550ca86ed47c487c0b5f742016d69b853c4c16
Gustavo Passos <theguspassos@gmail.com>
2020-04-16 13:13:06 -0300

    Updates

---
+++ portfolio/precache-manifest.584f1e2c1994995ed6dc454db8ff0cb9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f74160c6726c16feca4bccc3fc411de7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1a34b274a0a0e0f4719a",
+    "url": "/portfolio/static/css/2.735a36f9.chunk.css"
+  },
+  {
+    "revision": "90f4b6a6904b216ded9b",
+    "url": "/portfolio/static/css/main.523c3f65.chunk.css"
+  },
+  {
+    "revision": "1a34b274a0a0e0f4719a",
+    "url": "/portfolio/static/js/2.cd63d262.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.cd63d262.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90f4b6a6904b216ded9b",
+    "url": "/portfolio/static/js/main.f6a57232.chunk.js"
+  },
+  {
+    "revision": "210b9cd0d0953edddea9",
+    "url": "/portfolio/static/js/runtime-main.1b3815a1.js"
+  },
+  {
+    "revision": "de40467c81fa28189c2ae649c9d291dd",
+    "url": "/portfolio/static/media/EubyKuma.de40467c.png"
+  },
+  {
+    "revision": "9f96cfd196b18ce38995bec84b0bd2c8",
+    "url": "/portfolio/static/media/github.9f96cfd1.png"
+  },
+  {
+    "revision": "62549d5c53693231006fa6bb8705c65d",
+    "url": "/portfolio/static/media/gmail.62549d5c.png"
+  },
+  {
+    "revision": "072650574e041bf951d5e4e5ce07d86f",
+    "url": "/portfolio/static/media/linkedin.07265057.png"
+  },
+  {
+    "revision": "86fcbb81497b13f0c7a0c5025d50c354",
+    "url": "/portfolio/static/media/sketch-carousel-big.86fcbb81.png"
+  },
+  {
+    "revision": "7b5eb98fdd85f38ca80f8dfb3c2736a7",
+    "url": "/portfolio/static/media/sketch-carousel.7b5eb98f.png"
+  }
+]);
