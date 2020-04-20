12b314e2678af0d7aadb9c317d185fcfefa96198
Gustavo Passos <theguspassos@gmail.com>
2020-04-19 21:07:51 -0300

    Setup right projects in project list

--- src/data/projects/projectList.ts
+++ src/data/projects/projectList.ts
@@ -2,13 +2,7 @@ import Project from "../../models/project";
 import { portfolio } from "./portfolio/portfolio";
 import { sketch } from "./unity-state-machine/unityStateMachine";
 
-export const projectList: Project[] = [
-  portfolio,
-  portfolio,
-  portfolio,
-  sketch,
-  sketch,
-];
+export const projectList: Project[] = [portfolio, sketch];
 
 export const getProjectById = (id: number) => {
   return projectList.filter((a) => a.id === id)[0];
