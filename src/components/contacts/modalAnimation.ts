e3d3528990b165ccafcb8242caacaa3d5a1c7713
Gustavo Passos <theguspassos@gmail.com>
2020-07-17 14:02:28 -0300

    add animation to text

--- src/components/contacts/modalAnimation.ts
+++ src/components/contacts/modalAnimation.ts
@@ -4,39 +4,18 @@ import { animationSpeed } from "../../animations/animations";
 export const modalAnimation = css`
   &.animation-enter {
     opacity: 0;
-
-    > div:first-child {
-      transform: translate(0, 100%);
-    }
   }
-
   &.animation-enter-active {
     opacity: 1;
     transition: opacity ${animationSpeed}ms;
-
-    > div:first-child {
-      transform: translate(0, 0);
-      transition-timing-function: ease-in-out;
-      transition: transform ${animationSpeed}ms;
-    }
   }
 
   &.animation-exit {
     opacity: 1;
-
-    > div:first-child {
-      transform: translate(0, 0);
-    }
   }
 
   &.animation-exit-active {
     opacity: 0;
     transition: opacity ${animationSpeed}ms;
-
-    > div:first-child {
-      transform: translate(0, 100%);
-      transition-timing-function: ease-in-out;
-      transition: transform ${animationSpeed}ms;
-    }
   }
 `;
--- src/components/home/specifics/BioSection.tsx
+++ src/components/home/specifics/BioSection.tsx
@@ -1,6 +1,30 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import styled, { css } from "styled-components";
 import { sectionWithText } from "../../common-styles/sectionWithText";
+import { modalAnimation } from "../../contacts/modalAnimation";
+import { animationSpeed } from "../../../animations/animations";
+import { CSSTransition } from "react-transition-group";
+
+const fadeTime = 2000;
+
+const fadeAnimation = css`
+  &.animation-enter {
+    opacity: 0;
+  }
+  &.animation-enter-active {
+    opacity: 1;
+    transition: opacity ${fadeTime}ms;
+  }
+
+  &.animation-exit {
+    opacity: 1;
+  }
+
+  &.animation-exit-active {
+    opacity: 0;
+    transition: opacity ${fadeTime}ms;
+  }
+`;
 
 const BioSectionStyled = styled.section`
   color: #fff;
@@ -8,25 +32,39 @@ const BioSectionStyled = styled.section`
   margin: auto;
 
   ${sectionWithText};
+  ${fadeAnimation};
 `;
 
 const BioSection = () => {
+  const [inProp, setInProp] = useState(false);
+
+  useEffect(() => {
+    setInProp(true);
+  });
+
   return (
-    <BioSectionStyled>
-      <p>
-        I'm a 23 years old developer who's been delivering software
-        professionally for <b>6 years</b>. Interested in developing any kind of{" "}
-        <b>software or game</b>, regardless of the tech stack.
-      </p>
+    <CSSTransition
+      unmountOnExit
+      in={inProp}
+      classNames="animation"
+      timeout={fadeTime}
+    >
+      <BioSectionStyled>
+        <p>
+          I'm a 23 years old developer who's been delivering software
+          professionally for <b>6 years</b>. Interested in developing any kind
+          of <b>software or game</b>, regardless of the tech stack.
+        </p>
 
-      <p>
-        The <b>software quality</b> and <b>user experience</b> are the most
-        important things to me. I look forward to working with people that value
-        the same.
-      </p>
+        <p>
+          The <b>software quality</b> and <b>user experience</b> are the most
+          important things to me. I look forward to working with people that
+          value the same.
+        </p>
 
-      <p>If you need help in a project of yours feel free to contact me =)</p>
-    </BioSectionStyled>
+        <p>If you need help in a project of yours feel free to contact me =)</p>
+      </BioSectionStyled>
+    </CSSTransition>
   );
 };
 
