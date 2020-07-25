997f7cf97a681ff0aa23008a743100ee4224d56a
Gustavo Passos <theguspassos@gmail.com>
2020-07-25 13:28:58 -0300

    add game to portfolio

---
+++ src/data/projects/broken-delivery-service/broken-delivery-service.ts
@@ -0,0 +1,15 @@
+import Project from "../../../models/project";
+import img from "./broken-delivery-service.png";
+
+export const brokenDeliveryService: Project = {
+  id: 3,
+  name: "Broken Delivery Service",
+  description:
+    "Game made for the GMTK Jam in 42 hours. Made together with the Last Minute Team: @portluc and @flaviocravero.",
+  tags: ["unity"],
+  imageAlt: "scene from the broken delivery service game",
+  image: img,
+  madeWithText: "made with Unity",
+  linkButton: "play the game online",
+  linkToProject: "https://theguspassos.itch.io/broken-delivery-service",
+};
--- src/data/projects/projectList.ts
+++ src/data/projects/projectList.ts
@@ -1,8 +1,9 @@
 import Project from "../../models/project";
 import { portfolio } from "./portfolio/portfolio";
 import { blog } from "./blog/blog";
+import { brokenDeliveryService } from "./broken-delivery-service/broken-delivery-service";
 
-export const projectList: Project[] = [blog, portfolio];
+export const projectList: Project[] = [blog, portfolio, brokenDeliveryService];
 
 export const getProjectById = (id: number) => {
   return projectList.filter((a) => a.id === id)[0];
