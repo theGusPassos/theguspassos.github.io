87920c644e06016a41f0c1a9d6db720699ce4d1b
Gus <theguspassos@gmail.com>
2024-05-22 14:08:41 -0300

    add super dash project

--- src/data/projects/projectList.ts
+++ src/data/projects/projectList.ts
@@ -3,8 +3,10 @@ import { portfolio } from "./portfolio/portfolio";
 import { brokenDeliveryService } from "./broken-delivery-service/broken-delivery-service";
 import { slasherLock } from "./slasher-lock/slasher-lock";
 import { whoKilledGeorge } from "./who-killed-george/who-killed-george";
+import { superDash } from "./super-dash/super-dash";
 
 export const projectList: Project[] = [
+  superDash,
   whoKilledGeorge,
   slasherLock,
   brokenDeliveryService,
---
+++ src/data/projects/super-dash/super-dash.ts
@@ -0,0 +1,15 @@
+import Project from "../../../models/project";
+import img from "./super-dash-capsule.png";
+
+export const superDash: Project = {
+  id: 5,
+  name: "Super Dash",
+  description:
+    "A frenetic bullet-hell with roguelike elements. My first commercial project available in Steam.",
+  tags: ["unity"],
+  imageAlt: "super dash logo",
+  image: img,
+  madeWithText: "made with Unity",
+  linkButton: "steam page",
+  linkToProject: "https://store.steampowered.com/app/2726350/Super_Dash/",
+};
